26f412c015e3de9cda8291343f7e69d6db57bb70
Edi Carlos <ecarlosdasilva@byupathway.edu>
2026-05-08 18:42:30 -0300

    navigation done

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,7 @@
 const navButtom = document.querySelector("#ham-btn");
+const navBar = document.querySelector("#nav-bar");
 
 navButtom.addEventListener('click', () => {
     navButtom.classList.toggle('show');
+    navBar.classList.toggle('show');
 });
