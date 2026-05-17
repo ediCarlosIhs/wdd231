518c87849f092bccfd749ffbb0150edf06803aaa
Edi Carlos <ecarlosdasilva@byupathway.edu>
2026-05-16 22:58:53 -0300

    desktop view adjust

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -15,3 +15,69 @@ listButton.addEventListener("click", () => {
     gridButton.className = "";
     article.className = "list";
 });
+
+async function getMembers() {
+    try {
+        const response = await fetch(path);
+        const data = await response.json();
+        console.table(data.members);
+        displayMembers(data.members);
+    } catch (error) {
+        console.error("Error fetching members", error);
+        
+    }
+}
+
+const displayMembers = (members) => {
+
+    members.forEach((member) => {
+        
+        const companyImage = document.createElement("img");
+        companyImage.src = `./images/directory/${member.imagePath}`;
+        companyImage.setAttribute("loading", "lazy");
+        companyImage.setAttribute("width", "300");
+        companyImage.setAttribute("height", "200");
+        companyImage.setAttribute("alt", `${member.name} Company Image`);
+
+        const companyName = document.createElement("h2");
+        companyName.textContent = member.name;
+
+        const address = document.createElement("p");
+        address.textContent = member.address;
+
+        const phone = document.createElement("p");
+        phone.textContent = member.phone;
+
+        const website = document.createElement("a");
+        website.setAttribute("href", member.url);
+        website.setAttribute("target", "_blank");
+        website.textContent = "Website";
+
+        const memberLevel = document.createElement("p");
+        switch (member.level) {
+            case 1:
+                memberLevel.textContent = "Level = Member";
+                break;
+            case 2:
+                memberLevel.textContent = "Level = Silver";
+                break;
+            case 3:
+                memberLevel.textContent = "Level = Gold";
+                break;
+        }
+
+        const section = document.createElement("section");
+        section.appendChild(companyImage);
+        section.appendChild(companyName);
+        section.appendChild(address);
+        section.appendChild(phone);
+        section.appendChild(website);
+        section.appendChild(memberLevel);
+
+        article.appendChild(section);
+
+    });
+
+}
+
+getMembers();
