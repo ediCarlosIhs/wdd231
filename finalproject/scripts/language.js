c30c8bebbdb007a2b5610a71c4db4fbb5f79608e
Edi Carlos <ecarlosdasilva@byupathway.edu>
2026-06-16 15:16:18 -0300

    language selection

---
+++ finalproject/scripts/language.js
@@ -0,0 +1,10 @@
+const portugueseLanguage = document.querySelector("#pt-br");
+const englishLanguage = document.querySelector("#en");
+
+portugueseLanguage.addEventListener('click', () => {
+    console.log('portuguese language');
+})
+
+englishLanguage.addEventListener('click', () => {
+    console.log('English language');
+})
