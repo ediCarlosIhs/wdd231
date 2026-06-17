9dce0eecd6d81c738c78864c39289a9ae261ff66
Edi Carlos <ecarlosdasilva@byupathway.edu>
2026-06-17 13:07:55 -0300

    scroll adjustment for nav menu

--- finalproject/scripts/navigation.js
+++ finalproject/scripts/navigation.js
@@ -1,7 +1,9 @@
 const hamButton = document.querySelector("#ham-btn");
 const navBar = document.querySelector("#nav-bar");
+const body = document.body;
 
 hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('show');
     navBar.classList.toggle('show');
+    body.classList.toggle('body-no-scroll');
 });
