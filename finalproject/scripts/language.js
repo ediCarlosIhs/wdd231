3833d87c13cfe8514ff30728c3572222feaf5b84
Edi Carlos <ecarlosdasilva@byupathway.edu>
2026-06-16 21:50:32 -0300

    localstorage implementation

--- finalproject/scripts/language.js
+++ finalproject/scripts/language.js
@@ -1,10 +1,46 @@
 const portugueseLanguage = document.querySelector("#pt-br");
 const englishLanguage = document.querySelector("#en");
+const idiom = localStorage.getItem('language');
+
+// page elements
+const projectsLink = document.querySelector("#projectsLink");
+const aboutmeLinks = document.querySelectorAll(".aboutmeLink");
+const myDream = document.querySelector('.myDream');
+
+
 
 portugueseLanguage.addEventListener('click', () => {
-    console.log('portuguese language');
-})
+    // console.log('portuguese language');
+    if (!idiom)
+        localStorage.setItem('language', 'pt-br');
+
+    changeToPortuguese();
+
+});
 
 englishLanguage.addEventListener('click', () => {
-    console.log('English language');
+    // console.log('English language');
+    localStorage.removeItem('language');
+    changeToEnglish();
 })
+
+if (idiom) {
+    changeToPortuguese();
+}
+
+function changeToPortuguese() {
+    projectsLink.textContent = 'Projetos';
+    myDream.textContent = "Desenvolvedor de Software";
+
+    aboutmeLinks.forEach(link => {
+        link.textContent = "Sobre Mim";
+    })
+}
+
+function changeToEnglish() {
+    projectsLink.textContent = 'Projects';
+    myDream.textContent = "Software Developer";
+    aboutmeLinks.forEach(link => {
+        link.textContent = "About Me";
+    })
+}
